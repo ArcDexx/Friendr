1f72ade609f5c4d82259aafbe89bb7d78ce9aaf8
epita <epita@R1P1-37.local>
2016-12-16 20:42:22 +0100

    Podfile and project architecture added

---
+++ Friendr/Controllers/Login/FRLoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FRLoginViewController.swift
+//  Friendr
+//
+//  Created by epita on 16/12/16.
+//  Copyright © 2016 epita. All rights reserved.
+//
+
+import UIKit
+
+class FRLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
