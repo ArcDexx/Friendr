a76a09558e18a46c90f37394843ca04c579130b9
Dalila Watts <dalilawatts@vuzeonvuzeonmacbook-pro-de-dalila.home>
2016-12-24 17:08:00 +0100

    Facebook login functionnal, bringing to list view

--- Friendr/AppDelegate.swift
+++ Friendr/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
         return true
+    }
+    
+    public func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        return SDKApplicationDelegate.shared.application(application, open:url, sourceApplication:sourceApplication, annotation:annotation)
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
--- Friendr/Controllers/Login/FRLoginViewController.swift
+++ Friendr/Controllers/Login/FRLoginViewController.swift
@@ -10,25 +10,35 @@ import UIKit
 import FacebookLogin
 import FacebookCore
 
-class FRLoginViewController: UIViewController {
+class FRLoginViewController: UIViewController, LoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("viewDidLoad")
-        
-        if let accessToken = AccessToken.current {
-            print("accessToken not nil")
-            let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "FRUserListTableViewController") as! FRUserListTableViewController
-            
-            self.navigationController?.pushViewController(secondViewController, animated: true)
-        }
+    
         
         let loginButton = LoginButton(readPermissions: [.publicProfile ])
+        loginButton.delegate = self
         
         loginButton.frame = CGRect(x: 0,y: self.view.frame.height - 50 , width: self.view.frame.width , height: 50)
         
         view.addSubview(loginButton)
     }
-
+    
+    func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
+        switch result {
+        case .failed(let error):
+            print(error)
+        case .cancelled:
+            print("Cancelled")
+        default:
+            print("Logged In")
+            performSegue(withIdentifier: "toListFromLogin", sender: self)
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: LoginButton) {
+        print("Logged Out")
+    }
 }
